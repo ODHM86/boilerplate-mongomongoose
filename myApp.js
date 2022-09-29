bd47cabfc898c14a71dbbc8c4170befe1d4cc7fc
odhm86 <67576282+ODHM86@users.noreply.github.com>
2022-09-29 00:58:50 +0000

    final v

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 let mongoose = require('mongoose');
 
-mongoose.connect("process.env.MONGO_URI", { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // const Schema = mongoose.Schema;
 let personSchema = new mongoose.Schema({
